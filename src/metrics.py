f75043d4bcd8d6017200f91b4117c6e3c70adce1
danielhdz13 <55956673+danielhdz13@users.noreply.github.com>
2020-01-22 02:50:59 -0600

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -11,5 +11,11 @@ def mean_squared_error(estimates, targets):
     Implement this formula here, using numpy and return the computed MSE
 
     https://en.wikipedia.org/wiki/Mean_squared_error
-    """
-    raise NotImplementedError()
+    """ 
+    acc = 0
+    for i in range(len(estimates)):
+        diff = pow((targets[i] - estimates[i]), 2)
+        acc += diff
+    n = 1 / estimates.shape[0]
+    solution = n * acc
+    return solution
